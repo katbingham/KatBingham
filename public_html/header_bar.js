55d8c1ee1a1cceb96c3370ae5f0561d56875b4a9
Kathryn Bingham <kabing33@gmail.com>
2022-12-05 19:31:00 -0700

    Icons, Header Bar, Basic Contact Page, Thank You

---
+++ public_html/header_bar.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const e = React.createElement;
+
+class HeaderBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#header_bar_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(HeaderBar));
