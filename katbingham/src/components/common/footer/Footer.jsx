df9dee9da475ed3da189d86b5dc7f88958e3febe
Kathryn Bingham <kabing33@gmail.com>
2023-06-26 20:52:32 -0600

    Style change

--- katbingham/src/components/common/footer/Footer.jsx
+++ katbingham/src/components/common/footer/Footer.jsx
@@ -10,8 +10,8 @@ export default function Footer() {
         <div className="footer">
             <div className="social-links-parent">
                 <div className="social-links">
-                    <a id="github-link" target="_blank" href="https://github.com/katbingham" style={{marginRight: "10px"}}> <Github/></a> 
-                    <a id="linkedin-link" target="_blank" href="https://www.linkedin.com/in/kathryn-bingham-a56796a4/" style={{marginRight: "10px"}}> <Linkedin/></a>
+                    <a id="github-link" target="_blank" href="https://github.com/katbingham"> <Github/></a> 
+                    <a id="linkedin-link" target="_blank" href="https://www.linkedin.com/in/kathryn-bingham-a56796a4/"> <Linkedin/></a>
                     <a id="instagram-link" target="_blank" href="https://www.instagram.com/kat.bing/"> <Instagram/></a>
                 </div>
             </div>
